671d3fb93f5959c63623feb710d047ac03a7638e
ericminio <eric.mignot@gmail.com>
2013-02-02 17:01:21 -0500

    add incremental steps

--- test/primeFactors.spec.js
+++ test/primeFactors.spec.js
@@ -6,13 +6,18 @@ describe("Prime factors decomposer", function() {
 		expect(primeFactors(2)).toEqual([2]);
 	});
 
+	it("knows that 16 = 2 x 2 x 2 x 2", function() {
+		expect(primeFactors(16)).toEqual([2, 2, 2, 2]);
+	});
+
 	it("knows that 3 = 3", function() {
 		expect(primeFactors(3)).toEqual([3]);
 	});
 	
-	it("knows that 4 = 2 x 2", function() {
-		expect(primeFactors(4)).toEqual([2, 2]);
+	it("knows that 36 = 2 x 2 x 3 x 3", function() {
+		expect(primeFactors(36)).toEqual([2, 2, 3, 3]);
 	});
+
 
 	it("knows that 300 = 2 x 2 x 3 x 5 x 5", function() {
 		expect(primeFactors(300)).toEqual([2, 2, 3, 5, 5]);
